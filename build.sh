2f70dbb2d148a9e58bff02de008fab9bfa2b151f
nick_huang <nick_huang@accton.com>
2018-11-01 17:33:07 +0800

    Add content

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-gcc -o fun_p_a fun_p_a.c
-gcc -o sem_wait sem_wait.c -Wall -Werror -lpthread
-gcc -o mutex_wait mutex_wait.c -Wall -Werror -lpthread
-gcc -o sem_wait_process sem_wait_process.c -lpthread
-gcc -o link_list link_list.c
+gcc -g -o fun_p_a fun_p_a.c
+gcc -g -o sem_wait sem_wait.c -Wall -Werror -lpthread
+gcc -g -o mutex_wait mutex_wait.c -Wall -Werror -lpthread
+gcc -g -o sem_wait_process sem_wait_process.c -lpthread
+gcc -g -o link_list link_list.c
--- ch.sh
+++ ch.sh
@@ -1,5 +1,5 @@
 #git clone https://github.com/abrother94/C_Note.git
 git add *
 git add -u :/
-git commit -m '1st in'
+git commit -m 'Add content'
 git push -u origin master
