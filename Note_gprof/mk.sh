b146f5e99c39abb4589214e3296f562482ea33d3
abrother94 <abrother94@gmail.com>
2018-12-08 00:31:17 +0000

    Add content

--- Note_gprof/mk.sh
+++ Note_gprof/mk.sh
@@ -1,13 +1,25 @@
+#!/bin/bash
+
 # https://github.com/jrfonseca/gprof2dot
-# apt-get install python3 graphviz
-# 
-# pip install gprof2dot
+# sudo apt-get install python3 graphviz
+# sudo apt-get install python-pip 
+# sudo pip install gprof2dot
 
-gcc -g -pg -o test tset.c  
-./test
-gprof test gmon.out > report.txt
+rm_list=("test" "G" "output.png" "G.png" "gmon.out" "report.txt")
+rm_list_c=${#rm_list[@]}
+#echo "${rm_list_c}"
 
-#//or 
-
-gprof test | gprof2dot | dot -Tpng -o output.png
-
+if [ "$1" = "C" ];then
+    for i in "${rm_list[@]}"
+    do
+	`rm "$i"`
+    done
+else
+    gcc -g -pg -o test test.c  
+    g++ -g -pg -o G G.cpp 
+    ./test
+    ./G
+    #    gprof test gmon.out > report.txt
+    gprof test | gprof2dot | dot -Tpng -o output.png
+    gprof G | gprof2dot | dot -Tpng -o G.png
+fi
